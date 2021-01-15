1f103997c4f4b6c4fd4afc387545ab63946a7e30
lfontesm <moreirafontesleo@gmail.com>
2021-01-15 08:53:15 -0300

    Update my_mod.c

--- my_mod.c
+++ my_mod.c
@@ -1,13 +1,11 @@
-#include <linux/module.h>	// required for modules related stuffs
+#include <linux/module.h>	// required for module related stuff
  
-int __init mojule_init(void)
-{
+int __init mojule_init(void){
   printk("Module inserted \n");
   return 0;
 }
  
-void __exit mojule_deinit(void)
-{
+void __exit mojule_deinit(void){
   printk("Module removed \n");
 }
  
