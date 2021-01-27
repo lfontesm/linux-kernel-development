abec16f0dcc13a9bf2dd2bf1085e2414eaeaa126
Leonardo Fontes <moreirafontesleo@gmail.com>
2021-01-27 08:37:18 -0500

    Print the parent of the current process and it's PID

--- my_mod.c
+++ my_mod.c
@@ -1,7 +1,13 @@
 #include <linux/module.h>	// required for module related stuff
- 
+#include <linux/sched.h>
+#include <asm/current.h>
+
 int __init mojule_init(void){
-  printk("Module inserted \n");
+  struct task_struct *head = current->parent;
+
+  printk("Module insterted\n");
+  printk("%s[%d]\n", head->comm, head->pid);
+
   return 0;
 }
  
